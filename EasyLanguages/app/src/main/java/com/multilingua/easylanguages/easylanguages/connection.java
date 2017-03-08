0cb41222f8a005546a4395f433546998e14e7cac
Alexandre SCHANNE <alexandre.schanne@telecomanancy.eu>
2017-03-08 17:50:24 +0100

    mise à jour projet. Base de donnee interne avec Realm ==> creation de la classe Users ; Interface principale de connexion ; vue de connexion et d'inscription ; vue si connexion OK ; menu en classe mere herité de chaque classe ; pas de retour à l'écran de connexion si back une fois connecté.

---
+++ EasyLanguages/app/src/main/java/com/multilingua/easylanguages/easylanguages/connection.java
@@ -0,0 +1,8 @@
+package com.multilingua.easylanguages.easylanguages;
+
+/**
+ * Created by Alexandre on 26/01/2017.
+ */
+
+public class connection {
+}
---
+++ EasyLanguages/app/src/main/java/com/multilingua/easylanguages/easylanguages/inscription.java
@@ -0,0 +1,8 @@
+package com.multilingua.easylanguages.easylanguages;
+
+/**
+ * Created by Alexandre on 10/02/2017.
+ */
+
+public class inscription {
+}
